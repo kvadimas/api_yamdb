b9d885aa2c241c57f5cc2e5426c13bb0d4a336b4
Vadim Kurotkin <kvadimas1@gmail.com>
2023-04-15 02:24:28 +0300

    fix

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -8,8 +8,8 @@ app_name = 'api'
 # Создаётся роутер
 router_v1 = DefaultRouter()
 router_v1.register('titles', TitleViewSet)
-router_v1.register('category', CategoryViewSet)
-router_v1.register('genre', GenreViewSet)
+router_v1.register('categories', CategoryViewSet)
+router_v1.register('genres', GenreViewSet)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
