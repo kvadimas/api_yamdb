d0cd1f9abdaa5ba3204652e21db9795110b1a173
Vadim Kurotkin <kvadimas1@gmail.com>
2023-04-14 20:24:26 +0300

    Добавил жанр (Genre)

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,4 +1,4 @@
-from api.views import TitleViewSet
+from api.views import TitleViewSet, GenreViewSet
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
@@ -7,6 +7,7 @@ app_name = 'api'
 # Создаётся роутер
 router = DefaultRouter()
 router.register('titles', TitleViewSet)
+router.register('genre', GenreViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
