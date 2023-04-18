b0d3bd50ff16855ee18d63aaaf766a805285fda1
Vadim Kurotkin <kvadimas1@gmail.com>
2023-04-18 05:19:24 +0300

    Реализовано добавление таблиц для User, Category, Genre, GenreTitle, Title.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -24,9 +24,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'reviews.apps.ReviewsConfig',
-    'api.apps.ApiConfig',
-    'users.apps.UsersConfig',
+    'reviews',
+    'api',
+    'users',
 ]
 
 MIDDLEWARE = [
