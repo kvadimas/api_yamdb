a8c550c576dcda29b001e4b910d926dba916cde5
Vasilyev Aleksey <sjotik@yandex.ru>
2023-04-21 23:47:07 +0300

    импорты

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -100,7 +100,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    # !!! Если LimitOffsetPagination отрабатывает, то этот по умолчанию можно оставить
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
 }
